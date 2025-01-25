1186a6e909b15232a235dc174e4540b04e5267b4
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-01-25 17:38:35 +0800

    restart navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,45 +1,37 @@
 <template>
-    <nav>
+    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <!-- Logo Section -->
-            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap">
-                    Noted
-                </span>
-            </a>
-
-            <!-- Mobile Menu Button -->
-            <button id="triggerEl" data-collapse-toggle="navbar-default" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden"
-                aria-controls="navbar-default" aria-expanded="false" @click="toggleNavbar">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M1 1h15M1 7h15M1 13h15" />
-                </svg>
-            </button>
-
-            <!-- Navigation Links -->
-            <div ref="navbar" class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Noted</span>
+            </router-link>
+            <div class="flex md:hidden space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul
+                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <router-link to="/" class="nav-links block py-2 px-3 rounded-sm md:p-0">
+                        <router-link to="/" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
                             Home
                         </router-link>
                     </li>
                     <li>
-                        <router-link to="/projects" class="nav-links block py-2 px-3 rounded-sm md:border-0 md:p-0">
+                        <router-link to="/projects" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             Projects
                         </router-link>
                     </li>
                     <li>
-                        <router-link to="/login" class="nav-links block py-2 px-3 rounded-sm md:border-0 md:p-0">
+                        <router-link to="/login" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             Login
                         </router-link>
                     </li>
                     <li>
-                        <router-link to="/register" class="nav-links block py-2 px-3 rounded-sm md:border-0 md:p-0">
+                        <router-link to="/register" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             Register
                         </router-link>
                     </li>
@@ -53,7 +45,6 @@
 import { ref } from 'vue';
 
 const navbar = ref(null);
-const triggerEl = ref(null);
 
 const toggleNavbar = () => {
     if (navbar.value) {
@@ -63,6 +54,4 @@ const toggleNavbar = () => {
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
