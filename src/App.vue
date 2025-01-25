421f5c9b339e9e036d8cca76cb0fea8db3e4fbf0
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-01-25 15:41:54 +0800

    views and components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Navbar from './components/Navbar.vue'
+</script>
 
 <template>
-  <h1 class="text-red-500">Hello world!</h1>
+  <div class="min-h-screen flex flex-col bg-white">
+    <!-- Header -->
+    <header>
+      <Navbar />
+    </header>
+
+    <!-- Main Content -->
+    <main class="flex-grow container mx-auto p-4">
+      <router-view />
+    </main>
+
+    <!-- Footer -->
+    <!-- <footer class="bg-blue-600 text-white p-4 text-center">
+      <p>&copy; 2025 Noted. All rights reserved.</p>
+    </footer> -->
+  </div>
 </template>
 
 <style scoped></style>
